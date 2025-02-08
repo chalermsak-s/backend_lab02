2fc6ae8f7124fff3209e5dc5d52e592480979a85
chalermsak-s <kaidohjung@gmail.com>
2025-02-08 11:22:53 +0700

    Task 2.3

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 let x:number | string = 10;
 x = 'hello';
+if (typeof x === 'string') {
+   console.log('x is a string');
+}else if (typeof x === 'number') {
+   console.log('x is a number');
+}else {
+   console.log('x is neither a string nor a number');
+}
