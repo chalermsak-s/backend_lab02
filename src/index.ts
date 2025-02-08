69926a993668217c5e7fee8fbc30bb4098d7ecdc
chalermsak-s <kaidohjung@gmail.com>
2025-02-08 11:29:10 +0700

    Task 3.4

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-const add = (a:number,b:number) => {
-    return a+b;
- }
- const result = add(1,2) + 0;
- console.log(result, 'type of result:', typeof result);
- 
+const add = (a: number, b: number): string => {
+  const result = a + b;
+  return result.toString();
+};
+const result = add(1, 2) + 0;
+console.log(result, "type of result:", typeof result);
