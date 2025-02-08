cc96b19e97eb99ef51a5a8a3bb4104c08de40fd1
chalermsak-s <kaidohjung@gmail.com>
2025-02-08 11:53:09 +0700

    Task 4.7

---
+++ src/function2.ts
@@ -0,0 +1,5 @@
+const add = (a: number, b: number): string => {
+    const result = a + b;
+    return result.toString();
+  };
+  export default add;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import add, {subtract} from './funtions';
+import {subtract} from './funtions';
+import add from './function2';
 const result = add(1,2) + 0;
 const result2 = subtract(1,2) + 0;
 console.log(result, 'type of result:', typeof result);
