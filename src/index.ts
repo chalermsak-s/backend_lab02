513719259fc55ceab9902d92706b08edf42673e2
chalermsak-s <kaidohjung@gmail.com>
2025-02-08 11:48:17 +0700

    Task 4.6

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { add,subtract } from './funtions';
+import add, {subtract} from './funtions';
 const result = add(1,2) + 0;
 const result2 = subtract(1,2) + 0;
 console.log(result, 'type of result:', typeof result);
