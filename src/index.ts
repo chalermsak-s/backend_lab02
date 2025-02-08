8a5cd6da7838ea2f95432e4115bbcb6199b2fb12
chalermsak-s <kaidohjung@gmail.com>
2025-02-08 11:31:03 +0700

    Task 3.5

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 const add = (a: number, b: number): string => {
-  const result = a + b;
-  return result.toString();
-};
-const result = add(1, 2) + 0;
-console.log(result, "type of result:", typeof result);
+    const result = a + b;
+    return result.toString();
+  };
+  
+  const findMax = (numbers: number[]): string => {
+    const max = Math.max(...numbers);
+    return max.toString();
+  };
+  
+  const result = add(1, 2) + 0;
+  console.log(result, "type of result:", typeof result);
+  
+  const numbers = [1, 2, 3, 4, 5];
+  const maxNumber = findMax(numbers);
+  console.log("Max number:", maxNumber);
