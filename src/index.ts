0489b4f38b64f28c9620a0476de89b1c84660d95
chalermsak-s <kaidohjung@gmail.com>
2025-02-08 11:25:13 +0700

    Task 3.1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-let x:number | string = 10;
-x = 7;
-if (typeof x === 'string') {
-   console.log('x is a string');
-}else if (typeof x === 'number') {
-   console.log('x is a number');
-}else {
-   console.log('x is neither a string nor a number');
-}
+const add = (a:number,b:number) => {
+    return a+b;
+ }
+ console.log(add(1,2));
+ 
