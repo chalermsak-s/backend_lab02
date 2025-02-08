d3d76498e61846b1c46005f56c9c21a900c05d7f
chalermsak-s <kaidohjung@gmail.com>
2025-02-08 11:39:29 +0700

    Task 4.5

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
-const add = (a: number, b: number): string => {
-    const result = a + b;
-    return result.toString();
-  };
-  
-  const findMax = (numbers: number[]): string => {
-    const max = Math.max(...numbers);
-    return max.toString();
-  };
-  
-  const result = add(1, 2) + 0;
-  console.log(result, "type of result:", typeof result);
-  
-  const numbers = [1, 2, 3, 4, 5];
-  const maxNumber = findMax(numbers);
-  console.log("Max number:", maxNumber);
+import { add,subtract } from './funtions';
+const result = add(1,2) + 0;
+const result2 = subtract(1,2) + 0;
+console.log(result, 'type of result:', typeof result);
+console.log(result2, 'type of result2:', typeof result2);
+
